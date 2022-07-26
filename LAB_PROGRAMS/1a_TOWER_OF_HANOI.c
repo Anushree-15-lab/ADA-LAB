844ca61f450b9daa9888968096c4ad812678d35e
Anushree-15-lab <83897987+Anushree-15-lab@users.noreply.github.com>
2022-07-26 12:17:22 +0530

    Update 1a_TOWER_OF_HANOI.c

--- LAB_PROGRAMS/1a_TOWER_OF_HANOI.c
+++ LAB_PROGRAMS/1a_TOWER_OF_HANOI.c
@@ -1,20 +1,15 @@
 #include<stdio.h>
-void towerofhanoi(int n,char from,char to,char aux)
-{
-  if(n==1)
-  {
-    printf("\n Move disk 1 from %e to %e",from, to);
-    return;
-  }
-  towerofhanoi(n-1,from,aux,to);
-  printf("\nMove disk %d from %c to %c",n,from,to);
-  towerofhanoi(n-1,aux,to,from);
+void toh(int n,char a,char b,char c)
+{ if(n>0){ toh(n-1,a,c,b);
+printf("Move %c to %c",a,c);
+toh(n-1,b,a,c);
 }
- int main()
-{
-   int n;
-   printf("Enter number of disks:");
-   scanf("%d",&n);
-   towerofhanoi(n,'A','B','C');
-   return 0;
+}
+void main()
+{ char
+A,B,C; int
+num;
+printf("Enter number of disks: ");
+scanf("%d",&num);
+toh(num,'A','B','C');
 }
