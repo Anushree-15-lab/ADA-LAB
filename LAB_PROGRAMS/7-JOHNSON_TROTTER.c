1dcd24896febfbcb19fed4a6b11f9d44e1b7d9ec
Anushree-15-lab <83897987+Anushree-15-lab@users.noreply.github.com>
2022-07-27 11:40:52 +0530

    Update 7-JOHNSON_TROTTER.c

--- LAB_PROGRAMS/7-JOHNSON_TROTTER.c
+++ LAB_PROGRAMS/7-JOHNSON_TROTTER.c
@@ -1,65 +1,122 @@
-#include<stdio.h>
-#include<conio.h>
-int nn,i,count=0,p[100],pi[100],dir[100];
-void pperm()
-{
-    int i;
-    count=count+1;
-    printf("[%2d]",count);
-    for(i=1;i<=nn;++i)
-        printf("%d",p[i]);
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdbool.h>
 
+bool LEFT_TO_RIGHT = true;
+bool RIGHT_TO_LEFT = false;
+
+int searchArr(int a[], int n, int mobile)
+{
+    for (int i = 0; i < n; i++)
+        if (a[i] == mobile)
+           return i + 1;
 }
 
-void trans(int x,int y)
+int getMobile(int a[], bool dir[], int n)
 {
+    int mobile_prev = 0, mobile = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (dir[a[i]-1] == RIGHT_TO_LEFT && i!=0)
+        {
+            if (a[i] > a[i-1] && a[i] > mobile_prev)
+            {
+                mobile = a[i];
+                mobile_prev = mobile;
+            }
+        }
+        if (dir[a[i]-1] == LEFT_TO_RIGHT && i!=n-1)
+        {
+            if (a[i] > a[i+1] && a[i] > mobile_prev)
+            {
+                mobile = a[i];
+                mobile_prev = mobile;
+            }
+        }
+    }
 
-    printf("(%d %d)",x,y);
+    if (mobile == 0 && mobile_prev == 0)
+        return 0;
+    else
+        return mobile;
+}
+
+int printOnePerm(int a[], bool dir[], int n)
+{
+    int mobile = getMobile(a, dir, n);
+    int pos = searchArr(a, n, mobile);
+
+    if (dir[a[pos - 1] - 1] ==  RIGHT_TO_LEFT)
+       {
+           int temp=a[pos-1];
+           a[pos-1]=a[pos-2];
+           a[pos-2]=temp;
+       }
+
+    else if (dir[a[pos - 1] - 1] == LEFT_TO_RIGHT)
+       {
+           int temp=a[pos];
+           a[pos]=a[pos-1];
+           a[pos-1]=temp;
+       }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] > mobile)
+        {
+            if (dir[a[i] - 1] == LEFT_TO_RIGHT)
+                dir[a[i] - 1] = RIGHT_TO_LEFT;
+            else if (dir[a[i] - 1] == RIGHT_TO_LEFT)
+                dir[a[i] - 1] = LEFT_TO_RIGHT;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        printf("%d",a[i]);
     printf("\n");
 }
 
-void move(int x,int d)
-{
 
-    int z;
-    trans(pi[x],pi[x]+d);
-    z=p[pi[x]+d];
-    p[pi[x]]=z;
-    p[pi[x]+d]=x;
-    pi[z]=pi[x];
-    pi[x]=pi[x]+d;
+int fact(int n)
+{
+    int res = 1;
+    for (int i = 1; i <= n; i++)
+        res = res * i;
+    return res;
 }
 
-void perm(int n)
+
+void printPermutation(int n)
 {
 
-    int i;
-    if(n>nn)
-        pperm();
-    else
+    int a[n];
+
+
+    bool dir[n];
+
+
+    for (int i = 0; i < n; i++)
     {
-        pperm(n+1);
-        for(i=1;i<=n-1;++i)
-        {
-            move(n,dir[n]);
-            perm(n+1);
-        }
-        dir[n]=-dir[n];
+        a[i] = i + 1;
+        printf("%d",a[i]);
     }
+    printf("\n");
+
+
+    for (int i = 0; i < n; i++)
+        dir[i] =  RIGHT_TO_LEFT;
+
+    for (int i = 1; i < fact(n); i++)
+        printOnePerm(a, dir, n);
 }
+
+
 int main()
 {
-
-    printf("Enter the values of the n:");
-    scanf("%d",&nn);
-    printf("\n");
-    for(i=1;i<=nn;++i)
-    {
-        dir[i]=-1;
-        p[i]=i;
-        pi[i]=i;
-    }
-   perm(1);
-   printf("\n");
-   return 0;
+    int n;
+    printf("\nEnter number of digits: ");
+    scanf("%d",&n);
+    printPermutation(n);
+    return 0;
 }
